aba21651c8a56feb833d515f9bddcfe851f5828c
Arkana-05 <arkajasmine2@gmail.com>
2024-12-19 21:11:29 +0700

    Update mainHome.py

--- mainHome.py
+++ mainHome.py
@@ -3,7 +3,7 @@ garis = ("=")*60
 
 print(garis) #menampilkan isi dari variable garis
 print("Selamat Datang".upper().center(60)) # upper() dapat mengubah teks menjadi kapital semua, dan center untuk mengubah teks menjadi di tengah
-print("Admin PT DIGITAL IT PROGRAM".center(60))
+print("Admin PT RADAR IT".center(60))
 print(garis) #menampilkan isi dari variable garis
 print() #code untuk memberi jarak sebanyak 1 kali enter
 
